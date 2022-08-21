35ee2b4b067b2419a49da1e32c3263c1b9cf4ffb
HarveyP123 <64821114+HarveyP123@users.noreply.github.com>
2022-08-20 22:14:08 -0400

    Add files via upload

---
+++ SLR_Link_Pred/bash_scripts/ia_email_dense/run_slr_training.sh
@@ -0,0 +1,101 @@
+cd /home/hop20001/ADMM_SLR_GNN
+chmod +x main_admm.py
+
+export RWEPOCH=10
+export RTEPOCH=10
+export MOD_IN=ia_email_dense_HID128.ckpt
+export DAST=ia_email_dense
+export OPT=savlr
+export INIT_S=0.01
+export RHO=0.01
+export GPU=3
+
+export FC1=0.125
+export FC2=0.125
+nohup python main_admm.py --fc1weight ${FC1} --fc2weight ${FC2} --epochs ${RWEPOCH} \
+        --retrain-epoch ${RTEPOCH} --gpus ${GPU} --load-model ${MOD_IN} \
+        --dataset ${DAST} --log-name ${DAST}_${OPT}_FC1_${FC1}_FC2_${FC2}_checkacc \
+        --check-hardprune-acc --optimization ${OPT} --initial_s ${INIT_S} --rho ${RHO}&
+
+export FC1=0.25
+export FC2=0.25
+nohup python main_admm.py --fc1weight ${FC1} --fc2weight ${FC2} --epochs ${RWEPOCH} \
+        --retrain-epoch ${RTEPOCH} --gpus ${GPU} --load-model ${MOD_IN} \
+        --dataset ${DAST} --log-name ${DAST}_${OPT}_FC1_${FC1}_FC2_${FC2}_checkacc \
+        --check-hardprune-acc --optimization ${OPT} --initial_s ${INIT_S} --rho ${RHO}&
+
+export FC1=0.375
+export FC2=0.375
+nohup python main_admm.py --fc1weight ${FC1} --fc2weight ${FC2} --epochs ${RWEPOCH} \
+        --retrain-epoch ${RTEPOCH} --gpus ${GPU} --load-model ${MOD_IN} \
+        --dataset ${DAST} --log-name ${DAST}_${OPT}_FC1_${FC1}_FC2_${FC2}_checkacc \
+        --check-hardprune-acc --optimization ${OPT} --initial_s ${INIT_S} --rho ${RHO}&
+
+export FC1=0.5
+export FC2=0.5
+nohup python main_admm.py --fc1weight ${FC1} --fc2weight ${FC2} --epochs ${RWEPOCH} \
+        --retrain-epoch ${RTEPOCH} --gpus ${GPU} --load-model ${MOD_IN} \
+        --dataset ${DAST} --log-name ${DAST}_${OPT}_FC1_${FC1}_FC2_${FC2}_checkacc \
+        --check-hardprune-acc --optimization ${OPT} --initial_s ${INIT_S} --rho ${RHO}&
+
+export GPU=4
+
+export FC1=0.625
+export FC2=0.625
+nohup python main_admm.py --fc1weight ${FC1} --fc2weight ${FC2} --epochs ${RWEPOCH} \
+        --retrain-epoch ${RTEPOCH} --gpus ${GPU} --load-model ${MOD_IN} \
+        --dataset ${DAST} --log-name ${DAST}_${OPT}_FC1_${FC1}_FC2_${FC2}_checkacc \
+        --check-hardprune-acc --optimization ${OPT} --initial_s ${INIT_S} --rho ${RHO}&
+
+export FC1=0.75
+export FC2=0.75
+nohup python main_admm.py --fc1weight ${FC1} --fc2weight ${FC2} --epochs ${RWEPOCH} \
+        --retrain-epoch ${RTEPOCH} --gpus ${GPU} --load-model ${MOD_IN} \
+        --dataset ${DAST} --log-name ${DAST}_${OPT}_FC1_${FC1}_FC2_${FC2}_checkacc \
+        --check-hardprune-acc --optimization ${OPT} --initial_s ${INIT_S} --rho ${RHO}&
+
+export FC1=0.875
+export FC2=0.875
+nohup python main_admm.py --fc1weight ${FC1} --fc2weight ${FC2} --epochs ${RWEPOCH} \
+        --retrain-epoch ${RTEPOCH} --gpus ${GPU} --load-model ${MOD_IN} \
+        --dataset ${DAST} --log-name ${DAST}_${OPT}_FC1_${FC1}_FC2_${FC2}_checkacc \
+        --check-hardprune-acc --optimization ${OPT} --initial_s ${INIT_S} --rho ${RHO}&
+
+
+export FC1=0.90625
+export FC2=0.90625
+nohup python main_admm.py --fc1weight ${FC1} --fc2weight ${FC2} --epochs ${RWEPOCH} \
+        --retrain-epoch ${RTEPOCH} --gpus ${GPU} --load-model ${MOD_IN} \
+        --dataset ${DAST} --log-name ${DAST}_${OPT}_FC1_${FC1}_FC2_${FC2}_checkacc \
+        --check-hardprune-acc --optimization ${OPT} --initial_s ${INIT_S} --rho ${RHO}&
+
+export GPU=5
+# export CUDA_VISIBLE_DEVICES=4
+
+export FC1=0.9375
+export FC2=0.9375
+nohup python main_admm.py --fc1weight ${FC1} --fc2weight ${FC2} --epochs ${RWEPOCH} \
+        --retrain-epoch ${RTEPOCH} --gpus ${GPU} --load-model ${MOD_IN} \
+        --dataset ${DAST} --log-name ${DAST}_${OPT}_FC1_${FC1}_FC2_${FC2}_checkacc \
+        --check-hardprune-acc --optimization ${OPT} --initial_s ${INIT_S} --rho ${RHO}&
+
+export FC1=0.96875
+export FC2=0.96875
+nohup python main_admm.py --fc1weight ${FC1} --fc2weight ${FC2} --epochs ${RWEPOCH} \
+        --retrain-epoch ${RTEPOCH} --gpus ${GPU} --load-model ${MOD_IN} \
+        --dataset ${DAST} --log-name ${DAST}_${OPT}_FC1_${FC1}_FC2_${FC2}_checkacc \
+        --check-hardprune-acc --optimization ${OPT} --initial_s ${INIT_S} --rho ${RHO}&
+
+export FC1=0.984375
+export FC2=0.96875
+nohup python main_admm.py --fc1weight ${FC1} --fc2weight ${FC2} --epochs ${RWEPOCH} \
+        --retrain-epoch ${RTEPOCH} --gpus ${GPU} --load-model ${MOD_IN} \
+        --dataset ${DAST} --log-name ${DAST}_${OPT}_FC1_${FC1}_FC2_${FC2}_checkacc \
+        --check-hardprune-acc --optimization ${OPT} --initial_s ${INIT_S} --rho ${RHO}&
+
+export FC1=0.9921875
+export FC2=0.96875
+nohup python main_admm.py --fc1weight ${FC1} --fc2weight ${FC2} --epochs ${RWEPOCH} \
+        --retrain-epoch ${RTEPOCH} --gpus ${GPU} --load-model ${MOD_IN} \
+        --dataset ${DAST} --log-name ${DAST}_${OPT}_FC1_${FC1}_FC2_${FC2}_checkacc \
+        --check-hardprune-acc --optimization ${OPT} --initial_s ${INIT_S} --rho ${RHO}&
---
+++ SLR_Link_Pred/bash_scripts/ia_email_pruned/run_dense_training.sh
@@ -0,0 +1,69 @@
+cd /home/hop20001/ADMM_SLR_GNN
+chmod +x main_dense.py
+export EPOCH=20
+export LR_DC=0.98
+export DTSTNAME=ia_email_pruned
+export GPU=2
+export HID=4
+nohup python main_dense.py --input-size 16 --hidden-size ${HID} --num-classes 1 --num-epochs ${EPOCH} \
+      --batch-size 1024 --learning-rate 0.05 --gpus ${GPU} --running-dir /home/hop20001/ADMM_SLR_GNN \
+      --dataset ${DTSTNAME} --model-name ${DTSTNAME}_HID${HID} --lr-decay ${LR_DC} \
+      --log-name ${DTSTNAME}_train_HID${HID} &
+export HID=8
+nohup python main_dense.py --input-size 16 --hidden-size ${HID} --num-classes 1 --num-epochs ${EPOCH} \
+      --batch-size 1024 --learning-rate 0.05 --gpus ${GPU} --running-dir /home/hop20001/ADMM_SLR_GNN \
+      --dataset ${DTSTNAME} --model-name ${DTSTNAME}_HID${HID} --lr-decay ${LR_DC} \
+      --log-name ${DTSTNAME}_train_HID${HID} &
+export HID=12
+nohup python main_dense.py --input-size 16 --hidden-size ${HID} --num-classes 1 --num-epochs ${EPOCH} \
+      --batch-size 1024 --learning-rate 0.05 --gpus ${GPU} --running-dir /home/hop20001/ADMM_SLR_GNN \
+      --dataset ${DTSTNAME} --model-name ${DTSTNAME}_HID${HID} --lr-decay ${LR_DC} \
+      --log-name ${DTSTNAME}_train_HID${HID} &
+export HID=16
+nohup python main_dense.py --input-size 16 --hidden-size ${HID} --num-classes 1 --num-epochs ${EPOCH} \
+      --batch-size 1024 --learning-rate 0.05 --gpus ${GPU} --running-dir /home/hop20001/ADMM_SLR_GNN \
+      --dataset ${DTSTNAME} --model-name ${DTSTNAME}_HID${HID} --lr-decay ${LR_DC} \
+      --log-name ${DTSTNAME}_train_HID${HID} &
+
+export GPU=3
+
+export HID=32
+nohup python main_dense.py --input-size 16 --hidden-size ${HID} --num-classes 1 --num-epochs ${EPOCH} \
+      --batch-size 1024 --learning-rate 0.05 --gpus ${GPU} --running-dir /home/hop20001/ADMM_SLR_GNN \
+      --dataset ${DTSTNAME} --model-name ${DTSTNAME}_HID${HID} --lr-decay ${LR_DC} \
+      --log-name ${DTSTNAME}_train_HID${HID} &
+export HID=48
+nohup python main_dense.py --input-size 16 --hidden-size ${HID} --num-classes 1 --num-epochs ${EPOCH} \
+      --batch-size 1024 --learning-rate 0.05 --gpus ${GPU} --running-dir /home/hop20001/ADMM_SLR_GNN \
+      --dataset ${DTSTNAME} --model-name ${DTSTNAME}_HID${HID} --lr-decay ${LR_DC} \
+      --log-name ${DTSTNAME}_train_HID${HID} &
+export HID=64
+nohup python main_dense.py --input-size 16 --hidden-size ${HID} --num-classes 1 --num-epochs ${EPOCH} \
+      --batch-size 1024 --learning-rate 0.05 --gpus ${GPU} --running-dir /home/hop20001/ADMM_SLR_GNN \
+      --dataset ${DTSTNAME} --model-name ${DTSTNAME}_HID${HID} --lr-decay ${LR_DC} \
+      --log-name ${DTSTNAME}_train_HID${HID} &
+
+export GPU=4
+
+export HID=80
+nohup python main_dense.py --input-size 16 --hidden-size ${HID} --num-classes 1 --num-epochs ${EPOCH} \
+      --batch-size 1024 --learning-rate 0.05 --gpus ${GPU} --running-dir /home/hop20001/ADMM_SLR_GNN \
+      --dataset ${DTSTNAME} --model-name ${DTSTNAME}_HID${HID} --lr-decay ${LR_DC} \
+      --log-name ${DTSTNAME}_train_HID${HID} &
+export HID=96
+nohup python main_dense.py --input-size 16 --hidden-size ${HID} --num-classes 1 --num-epochs ${EPOCH} \
+      --batch-size 1024 --learning-rate 0.05 --gpus ${GPU} --running-dir /home/hop20001/ADMM_SLR_GNN \
+      --dataset ${DTSTNAME} --model-name ${DTSTNAME}_HID${HID} --lr-decay ${LR_DC} \
+      --log-name ${DTSTNAME}_train_HID${HID} &
+export HID=112
+nohup python main_dense.py --input-size 16 --hidden-size ${HID} --num-classes 1 --num-epochs ${EPOCH} \
+      --batch-size 1024 --learning-rate 0.05 --gpus ${GPU} --running-dir /home/hop20001/ADMM_SLR_GNN \
+      --dataset ${DTSTNAME} --model-name ${DTSTNAME}_HID${HID} --lr-decay ${LR_DC} \
+      --log-name ${DTSTNAME}_train_HID${HID} &
+
+export GPU=2
+export HID=128
+nohup python main_dense.py --input-size 16 --hidden-size ${HID} --num-classes 1 --num-epochs ${EPOCH} \
+      --batch-size 1024 --learning-rate 0.05 --gpus ${GPU} --running-dir /home/hop20001/ADMM_SLR_GNN \
+      --dataset ${DTSTNAME} --model-name ${DTSTNAME}_HID${HID} --lr-decay ${LR_DC} \
+      --log-name ${DTSTNAME}_train_HID${HID} &
